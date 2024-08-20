e1988bf76099c221cb6153c8818d6f4195bc2ffb
Sano <36809574+Sanokei@users.noreply.github.com>
2024-08-20 01:52:44 -0400

    adding city

--- Assets/Scripts/Menu/MenuCameraController.cs
+++ Assets/Scripts/Menu/MenuCameraController.cs
@@ -23,10 +23,10 @@ public class MenuCameraController : MonoBehaviour
         _Ocean.Priority = 0;
         _Dark.Priority = 1;
         _QueuedSceneName = sceneName;
-        this.Delay(5f,() =>
+        this.Delay(4f,() =>
         {
             _BubbleTransition.Play();
-            StartCoroutine(ChangeColor(new Color(0.08810966f, 0.1572813f, 0.4150943f, 1f),Color.black,4.5f,() => SceneManager.LoadScene(_QueuedSceneName)));
+            StartCoroutine(ChangeColor(new Color(0.08810966f, 0.1572813f, 0.4150943f, 1f),Color.black,7f,() => SceneManager.LoadScene(_QueuedSceneName)));
         }
         );
     }
